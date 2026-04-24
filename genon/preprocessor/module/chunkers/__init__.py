951e3bca4f28bdf3dafc5e9c04543bce7d14c119
seongmincho315 <seongmin.cho@genon.ai>
2026-04-24 16:44:38 +0900

    feat: 글자수 기반 청커

--- genon/preprocessor/module/chunkers/__init__.py
+++ genon/preprocessor/module/chunkers/__init__.py
@@ -1,9 +1,11 @@
 from .GenosBucketChunker import GenosBucketChunker
+from .GenosCharacterBucketChunker import GenosCharacterBucketChunker
 from .SimpleChunker import SimpleChunker
 
 CHUNKERS = {
     "bucket": GenosBucketChunker,
+    "char_bucket": GenosCharacterBucketChunker,
     "simple": SimpleChunker,
 }
 
-__all__ = ["GenosBucketChunker", "SimpleChunker", "CHUNKERS"]
+__all__ = ["GenosBucketChunker", "GenosCharacterBucketChunker", "SimpleChunker", "CHUNKERS"]
