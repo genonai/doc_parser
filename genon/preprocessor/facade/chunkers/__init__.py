125c494c09cc3855cd3e234ce3ee067543c54c30
seongmincho315 <seongmin.cho@genon.ai>
2026-04-30 13:39:34 +0900

    chore: 청커 정리

--- genon/preprocessor/facade/chunkers/__init__.py
+++ genon/preprocessor/facade/chunkers/__init__.py
@@ -1,11 +1,7 @@
 from .GenosBucketChunker import GenosBucketChunker
-from .GenosCharacterBucketChunker import GenosCharacterBucketChunker
-from .SimpleChunker import SimpleChunker
 
 CHUNKERS = {
     "bucket": GenosBucketChunker,
-    "char_bucket": GenosCharacterBucketChunker,
-    "simple": SimpleChunker,
 }
 
-__all__ = ["GenosBucketChunker", "GenosCharacterBucketChunker", "SimpleChunker", "CHUNKERS"]
+__all__ = ["GenosBucketChunker"]
