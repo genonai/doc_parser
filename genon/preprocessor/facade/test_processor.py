0b749025d209e8e451c2020b12c0c08d020886b8
seongmincho315 <seongmin.cho@genon.ai>
2026-05-20 14:25:36 +0900

    feat: config

--- genon/preprocessor/facade/test_processor.py
+++ genon/preprocessor/facade/test_processor.py
@@ -1,11 +1,8 @@
-from pathlib import Path
-
 import yaml
 
 from genon.preprocessor.facade.base_processor import BaseProcessor
 
-_HERE = Path(__file__).parent
-config = yaml.safe_load((_HERE / "intelligent_config.yaml").read_text(encoding="utf-8"))
+config = yaml.safe_load(("/app/resource/config.yaml").read_text(encoding="utf-8"))
 
 
 class DocumentProcessor(BaseProcessor):
